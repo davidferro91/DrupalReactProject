70e87bb44b2722d5f1ff48e2cadae1c73a38d3fb
Rahul Raj <rahulrajshopping@icloud.com>
2019-04-02 20:05:39 -0400

    added code for sidebar

--- drupal-case-react-project/src/App.js
+++ drupal-case-react-project/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 class App extends Component {
   render() {
     return (
-      <Navbar></Navbar>
+      <Sidebar />
     );
   }
 }
---
+++ drupal-case-react-project/src/components/SidebarListElem/ListElem.js
@@ -0,0 +1,83 @@
+import React, {Component} from 'react';
+import './ListElem.css';
+
+class ListElem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isClicked: false};
+        this.changeToActive = this.changeToActive.bind(this);
+    }
+
+    changeToActive = (e) => {
+        e.preventDefault();
+        this.setState({
+            isClicked: !this.state.isClicked
+        });
+    }
+
+    render() {
+        return (
+
+            <div>
+
+            {/* we should use an array loop here, but for now I will just hard code the list elements */}
+            <li>
+                <a
+                href={this.props.href} 
+                className={this.state.isClicked ? 'nav-link active' : 'nav-link'}
+                onClick={this.changeToActive}
+                >
+                Link
+                </a>
+            </li>
+
+            <li>
+                <a
+                href={this.props.href} 
+                className={this.state.isClicked ? 'nav-link active' : 'nav-link'}
+                onClick={this.changeToActive}
+                >
+                Link
+                </a>
+            </li>
+
+            <li>
+                <a
+                href={this.props.href} 
+                className={this.state.isClicked ? 'nav-link active' : 'nav-link'}
+                onClick={this.changeToActive}
+                >
+                Link
+                </a>
+            </li>
+
+
+            <li>
+                <a
+                href={this.props.href} 
+                className={this.state.isClicked ? 'nav-link active' : 'nav-link'}
+                onClick={this.changeToActive}
+                >
+                Link
+                </a>
+            </li>
+
+
+            <li>
+                <a
+                href={this.props.href} 
+                className={this.state.isClicked ? 'nav-link active' : 'nav-link'}
+                onClick={this.changeToActive}
+                >
+                Link
+                </a>
+            </li>
+
+           
+            </div>
+
+        )
+    }
+}
+
+export default ListElem;
