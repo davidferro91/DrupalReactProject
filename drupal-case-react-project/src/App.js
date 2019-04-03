6459690df8b44b94301e53978fd4c5c5acf03fd4
davidferro <david-ferro@sbcglobal.net>
2019-04-02 21:40:44 -0400

    breadcrumbsgit add .! They are crumbly

--- drupal-case-react-project/src/App.js
+++ drupal-case-react-project/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Breadcrumbs from './components/Breadcrumbs';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Breadcrumbs  mainLink={"/research/about/research-centers-interdisciplinary-institutes"} mainTitle={"Research Centers and Interdisciplinary Institutes"} links={[
+          {
+            url: "/research",
+            title: "Home"
+          },
+          {
+            url: "/research/about",
+            title: "About"
+          }
+          ]}  />
         <Sidebar />
       </div>
     );
