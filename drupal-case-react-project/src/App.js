d7defedcc43fe2e45bf358b82373296086533fac
Rahul Raj <rahulrajshopping@icloud.com>
2019-04-04 19:16:33 -0400

    added jumbotron and changed background color

--- drupal-case-react-project/src/App.js
+++ drupal-case-react-project/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import "./app.css";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Carousel />
         <Sidebar />
       </div>
     );
