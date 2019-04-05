5843a6c087aae38ead112c383e3b5044a49ca302
Brett <benderb73@gmil.com>
2019-04-04 21:02:36 -0400

    footer-almost-done

--- drupal-case-react-project/src/components/Footer/index.js
+++ drupal-case-react-project/src/components/Footer/index.js
@@ -20,6 +20,43 @@ class Footer extends React.Component {
             </div>
             <div>
                 <span className={s.text}>© 2017 Case Western Reserve University, 10900 Euclid Ave., Cleveland, Ohio 44106 216.368.2000</span>
+               <table className="data">
+                <tr>
+                  <td className="data"><span className="data">Campus Safety</span></td>
+                    <br></br>
+                  <td>Libraries</td>
+                </tr>
+                <tr>
+                  <td>Canvas</td>
+                  <br></br>
+                  <td>Media</td>
+                </tr>
+                <tr>
+                  <td>Centers + Institutes</td>
+                    <br></br>
+                  <td>Parking + Shuttles</td>
+                </tr>
+                <tr>
+                  <td>Directories</td>
+                    <br></br>
+                  <td>Student Inforamtion System</td>
+                </tr>
+                <tr>
+                  <td>Diversity</td>
+                  <br></br>
+                  <td>University Technology</td>
+                </tr>
+                <tr>
+                  <td>Financial Aid</td>
+                  <br></br>
+                  <td>Web Mail</td>
+                </tr>
+                <tr>
+                  <td>Give to CWRU</td>
+                  
+                  <td>Work at CWRU</td>
+                </tr>
+              </table>
             </div>
             </Col>
          
