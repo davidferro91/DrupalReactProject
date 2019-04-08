99778138d419396d934fe848838c707e91cd08cd
davidferro <david-ferro@sbcglobal.net>
2019-04-07 21:33:22 -0400

    Updates

---
+++ precache-manifest.12ef2aec97f486b52771b2584789e48a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b388f20c4d18db88a4e4d7ca7aad8d8c",
+    "url": "/DrupalReactProject/static/media/tyler-casey-1474093-unsplash.b388f20c.jpg"
+  },
+  {
+    "revision": "957f0174ff7fbe3bf65c2068b0cc470b",
+    "url": "/DrupalReactProject/static/media/cwru.957f0174.png"
+  },
+  {
+    "revision": "9529dec53a01c49d028eccb61425de31",
+    "url": "/DrupalReactProject/static/media/brennan-burling-1473464-unsplash.9529dec5.jpg"
+  },
+  {
+    "revision": "18dae91841598fab594a",
+    "url": "/DrupalReactProject/static/js/runtime~main.7e6388d1.js"
+  },
+  {
+    "revision": "c551282d08fe29ba2700",
+    "url": "/DrupalReactProject/static/js/main.28a11e89.chunk.js"
+  },
+  {
+    "revision": "3dd3369290c0587a9945",
+    "url": "/DrupalReactProject/static/js/2.ee42fed3.chunk.js"
+  },
+  {
+    "revision": "c551282d08fe29ba2700",
+    "url": "/DrupalReactProject/static/css/main.d988d945.chunk.css"
+  },
+  {
+    "revision": "d005dea60872f22d2ac37593d176eaeb",
+    "url": "/DrupalReactProject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/davidferro91/DrupalReactProject/precache-manifest.6c90827e8fff55ba4964d15bf8e7d996.js"
+  "/DrupalReactProject/precache-manifest.12ef2aec97f486b52771b2584789e48a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/davidferro91/DrupalReactProject/index.html", {
+workbox.routing.registerNavigationRoute("/DrupalReactProject/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
