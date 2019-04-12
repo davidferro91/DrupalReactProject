b75f88729964fd4831befed77a4f053d770c4f01
Rahul Raj <rahulrajshopping@icloud.com>
2019-04-12 16:20:05 -0400

    removed components from app and put an initial route

---
+++ drupal-case-react-project/src/pages/BootCampSched.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../components/BootCampLandingPageNav';
+import BootCampLandingPageNavbar from '../components/BootCampLandingPageNav';
+
+class BootCampSched extends Component {
+
+    render() {
+        return (
+            <div>
+                <BootCampLandingPageNavbar />
+            </div>
+        )
+    }
+   
+}
+
+export default BootCampSched;
