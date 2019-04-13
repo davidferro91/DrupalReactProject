7feca0aa4f7e58bbc8094614c4ac7751b42ef347
davidferro <david-ferro@sbcglobal.net>
2019-04-13 11:54:01 -0400

    made carousel prop

--- drupal-case-react-project/src/components/Dropdown/index.js
+++ drupal-case-react-project/src/components/Dropdown/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './style.css';
 import { Row, Col } from '../Grid';
+import { Link } from "react-router-dom";
+
 
 function Dropdown(props) {
   return (
     <li className="nav-item dropdown border border-white mx-0 pr-2" >
       <Row>
         <Col size={"sm-auto lg-9"}>
-          <a className="nav-link my-3 mr-1 p-2" href={props.mainLink} id="navbarDropdown">
+          <Link className="nav-link my-3 mr-1 p-2" to={props.mainLink} id="navbarDropdown">
             <strong className="text-wrap pr-2">{props.children}</strong>
-          </a>
+          </Link>
         </Col>
         <Col size={"xs-1"}>
           <i className="fas fa-angle-down py-4 mr-1 pr-1"></i>
@@ -19,7 +21,7 @@ function Dropdown(props) {
         {props.links.map(link => (
           <div key={link.title}>
             <div className="dropdown-divider m-0 p-0"></div>
-            <a className="dropdown-item p-3" href={link.url} key={link.title}>{link.title}</a>
+            <Link className="dropdown-item p-3" to={link.url} key={link.title}>{link.title}</Link>
             <div className="dropdown-divider m-0 p-0"></div>
           </div>
         ))}
--- drupal-case-react-project/src/components/NavLink/index.js
+++ drupal-case-react-project/src/components/NavLink/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function NavLink(props) {
   return (
     <li className="nav-item border border-white mx-0 pr-2">
-      <a className="nav-link my-3 mx-1 p-2" href={props.mainLink}><strong className="text-wrap pr-2">{props.children}</strong></a>
+      <Link className="nav-link my-3 mx-1 p-2" to={props.mainLink}><strong className="text-wrap pr-2">{props.children}</strong></Link>
     </li>
   );
 }
