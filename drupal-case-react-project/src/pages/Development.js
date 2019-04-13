c27b48158b498a66ad568756f2ef00d226edc16b
davidferro <david-ferro@sbcglobal.net>
2019-04-13 14:41:57 -0400

    making cards

--- drupal-case-react-project/src/pages/Development.js
+++ drupal-case-react-project/src/pages/Development.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar'
-import LogoTop from './components/LogoTop';
-import DepartmentHeader from './components/DepartmentHeader';
-import Jumbotron from './components/Jumbotron';
-import Carousel from './components/Carousel';
-import Breadcrumbs from './components/Breadcrumbs';
-import Table from './components/Table';
-import Wrapper from './components/Wrapper';
-import Footer from './components/Footer';
-import "./app.css";
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar'
+import LogoTop from '../components/LogoTop';
+import DepartmentHeader from '../components/DepartmentHeader';
+import Jumbotron from '../components/Jumbotron';
+// import Carousel from '../components/Carousel';
+import Breadcrumbs from '../components/Breadcrumbs';
+import Table from '../components/Table';
+import Wrapper from '../components/Wrapper';
+import Footer from '../components/Footer';
+import { TextCard, ImageCard } from '../components/Cards';
+import "../app.css";
 
 class Development extends Component {
   render() {
@@ -29,13 +30,20 @@ class Development extends Component {
               title: "About"
             }
           ]} />
-          <Carousel
+          <TextCard name="Test" linkName="Test Link" link="/about">
+          This is a test of the textcard.
+
+          </TextCard>
+          <ImageCard>
+
+          </ImageCard>
+          {/* <Carousel
             image1="image1link"
             image2="image2link"
             image3="image3link"
             header="headercontent"
             subheader="subheadercontent"
-          />
+          /> */}
           <Sidebar />
           <Jumbotron />
           <Table 
---
+++ drupal-case-react-project/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import LogoTop from '../components/LogoTop';
+import DepartmentHeader from '../components/DepartmentHeader';
+import Navbar from '../components/Navbar';
+import Wrapper from '../components/Wrapper';
+import Breadcrumbs from '../components/Breadcrumbs';
+import { Container, Row, Col } from '../components/Grid';
+import Sidebar from '../components/Sidebar';
+import { TextHolder, ImageHolder } from '../components/Holders';
+import Loader from '../components/Loader';
+import API from '../utils/API';
+import Footer from '../components/Footer';
+import '../app.css';
+
+class Home extends Component {
+
+  state = {
+    results: [],
+    images: [],
+    error: "",
+    title: "",
+    content: []
+  }
+
+  render () {
+
+    
+  }
+}
+
+export default Home;
