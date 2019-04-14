750a63cfdb3a2040e9683b30d91ef6c79e05e0b6
Rahul Raj <rahulrajshopping@icloud.com>
2019-04-14 19:25:22 -0400

    added prop functionality to sidebar

--- drupal-case-react-project/src/components/Sidebar/index.js
+++ drupal-case-react-project/src/components/Sidebar/index.js
@@ -5,32 +5,21 @@ import ListElem from '../SidebarListElem';
 
 class Sidebar extends Component {
 
-  state = {
-    listitems: []
-  }
-  
-  //put a componentDidMount function here that loads the data
-
   render() {
     return (
       <Col size={"lg-3 md-3 sm-12 xs-12"}>
         <nav className="" aria-label="Sidebar Menu">
           <div className="page-sidebar">
             <a href="demo_sidebar1.html">
-              <h2 className="sidebar-header">{this.props.sidebartitle}</h2>
+              <h2 className="sidebar-header">{this.props.title}</h2>
             </a>
-            {/*some sort of array state manipulation here depending on the php drupal data coming in*/}
+            {/*array state manipulation here depending on the php drupal data coming in*/}
             <ul className="page-sidebar-menu" >
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
-              <ListElem content="Testing" href='index.html'/>
+              {/*the array is passed in as a prop to the sidebar element
+              we can now loop through the array, create a list element for each element in the array 
+              and then populate the listitems content prop with the content from the corresponding element 
+              in the array*/}
+              <ListElem content={from array stuff above} href='index.html'/>
             </ul>
           </div>
         </nav>
